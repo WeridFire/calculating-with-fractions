6dc8d63a5342760fd98dab7a104fcb646721443a
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2022-12-19 18:36:36 +0100

    everything sorted up correctly

--- fraction.py
+++ fraction.py
@@ -1,7 +1,14 @@
 class Number:
 
-    def __init__(self, power=1):
+    def __init__(self, value, power=1):
+        self.value = value
         self.power = power
+
+    def calc_pow(self):
+        res = 1
+        for i in range(self.power):
+            res *= self.value
+        return res
 
 
 class Fraction:
@@ -16,6 +23,23 @@ class Fraction:
             print("{}/{}".format(self.num, self.den))
         else:
             print(self.num)
+
+    def simplify(self):
+
+        min_num = min(self.num,self.den)
+        max_num = max(self.num,self.den)
+
+        if max_num%min_num == 0:
+            d=min_num
+
+        else:
+            for i in range(int(min_num/2),1, -1):
+                if min_num%i == 0 and max_num%i==0:
+                    d=min_num
+
+        self.den = int(self.den/d)
+        self.num = int(self.num/d)
+
 
 
 
---
+++ test.py
@@ -0,0 +1,16 @@
+from fraction import *
+
+
+#tests
+fract = Fraction(121, 11)
+
+fract.show_fraction()
+
+fract.simplify()
+
+fract.show_fraction()
+
+a = Number(7,2)
+
+print(a.calc_pow())
+
