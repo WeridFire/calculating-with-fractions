7d2bf66aabe646a388ee7106e99cfaa4824eab2b
Maretti Filippo <69721649+WeridFire@users.noreply.github.com>
2022-12-19 18:12:36 +0100

    faster simplify funct

--- functs.py
+++ functs.py
@@ -1,8 +1,6 @@
 from fraction import *
 
-def find_max_divisors(a:int, b:int):
-
-    max_div = 1
+def find_max_divisor(a:int, b:int):
 
     min_num = min(a,b)
     max_num = max(a,b)
@@ -10,16 +8,14 @@ def find_max_divisors(a:int, b:int):
     if max_num%min_num == 0:
         return min_num
 
-    for i in range(1,int(min_num/2)):
+    for i in range(int(min_num/2),1, -1):
 
         if min_num%i == 0 and max_num%i==0:
-            max_div = i
-
-    return max_div
+            return i
 
 def simplify(n:Fraction):
 
-    d:int = find_max_divisors(n.num, n.den)
+    d:int = find_max_divisor(n.num, n.den)
 
     n.den = int(n.den/d)
     n.num = int(n.num/d)
@@ -27,7 +23,7 @@ def simplify(n:Fraction):
     return n
 
 
-fract = Fraction(898972,4)
+fract = Fraction(121, 11)
 
 fract.show_fraction()
 
